cfa91288437588b79985fbc6a987f7cc875c1966
Kurtmj93 <kurtmj93@gmail.com>
2023-02-20 22:36:22 -0500

    create server.js and add express GET routes

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+// require express & call as function
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
